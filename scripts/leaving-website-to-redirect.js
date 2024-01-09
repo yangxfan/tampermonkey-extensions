f53575212e1cca740a062d0d0a68457e0b518a66
yangxfan <18810287175@163.com>
2024-01-09 19:34:03 +0800

    first commit

---
+++ scripts/leaving-website-to-redirect.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         正在离开网站，即将跳转到外部网站
+// @namespace    https://github.com/yangxfan/tampermonkey-extensions
+// @version      1.0
+// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查
+// @author       yangxfan
+// @match        *://link.zhihu.com/*
+// @match        *://www.jianshu.com/go-wild*
+// @match        *://gitee.com/link*
+// @match        *://sspai.com/link*
+// @match        *://link.juejin.cn/*
+// @match        *://www.tianyancha.com/security/*
+// @grant        none
+// ==/UserScript==
+
+/**
+ * 2024-01-09
+ * 首次提交
+ */
+
+(() => {
+  if (URL) {
+    /**
+     * 网站跳转外部网站 url 的 key 值
+     * 知乎 target
+     * 简书 url
+     * 码云 link
+     * 少数派 target
+     * 掘金 target
+     * 天眼查 target
+     */
+    const keys = ['target', 'url', 'link']
+    // 获取到外部网站 url
+    const getParam = params => {
+      for (const i of keys) {
+        let link = params.get(i)
+        if (link) {
+          return link
+        }
+      }
+    }
+    const searchParams = new URL(location.href).searchParams
+    let link = getParam(searchParams)
+
+    if (link) {
+      location.href = link
+    }
+  }
+})()
