0ded21fce9aa65fd0a7d8983681c5f28c90cc416
yangxfan <18810287175@163.com>
2024-01-11 10:45:33 +0800

    feat: 知乎-去掉复制内容的版权声明

--- scripts/zhihu-remove-login.js
+++ scripts/zhihu-remove-login.js
@@ -1,13 +1,25 @@
 // ==UserScript==
 // @name         知乎去掉没有登录时的登录弹框
 // @namespace    https://github.com/yangxfan/tampermonkey-extensions
-// @version      0.1
-// @description  查看知乎文章时，不想登录，又不想看到登录弹框，又不想点击"x"关闭或者"ESC"关闭，所以写了样式隐藏登录弹框
+// @version      0.2
+// @description  查看知乎文章时，不想登录，又不想看到登录弹框，又不想点击"x"关闭或者"ESC"关闭，所以写了样式隐藏登录弹框；去掉复制内容的版权声明
 // @author       yangxfan
 // @match        *://*.zhihu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
 // @grant        none
 // ==/UserScript==
+
+/**
+ * 2024-01-10
+ * 0.2
+ * 去掉复制内容的版权声明
+ */
+
+/**
+ * 2024-01-09
+ * 0.1
+ * 首次提交
+ */
 
 (() => {
   const addStylesheetRules = () => {
@@ -19,4 +31,14 @@
   }
 
   addStylesheetRules()
+
+  setTimeout(() => {
+    // 去掉“复制字符大于 140 时，追加版权声明”逻辑，是通过替换节点移除事件监听来实现的
+    // 延迟时间，是因为解决网站打不开并显示“出了一点问题”
+    const oldContainer = document.querySelector('.RichText')
+    if (oldContainer) {
+      const newContainer = oldContainer.cloneNode(true)
+      oldContainer.parentNode.replaceChild(newContainer, oldContainer)
+    }
+  }, 100)
 })()
