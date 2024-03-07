decf4ca794b59e98f9011a8b101fffb6598f062c
yangxfan <18810287175@163.com>
2024-03-07 17:18:47 +0800

    update: 正在离开网站-添加腾讯文档

--- scripts/leaving-website-to-redirect.js
+++ scripts/leaving-website-to-redirect.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         正在离开网站，即将跳转到外部网站
 // @namespace    https://github.com/yangxfan/tampermonkey-extensions
-// @version      1.1
-// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN
+// @version      1.2
+// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN、腾讯文档
 // @author       yangxfan
 // @match        *://link.zhihu.com/*
 // @match        *://www.jianshu.com/go-wild*
@@ -11,12 +11,17 @@
 // @match        *://link.juejin.cn/*
 // @match        *://www.tianyancha.com/security/*
 // @match        *://link.csdn.net/?target=*
+// @match        *://docs.qq.com/scenario/link.html?url=
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // @downloadURL  https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // ==/UserScript==
 
 /**
+ * 2024-03-07
+ * 1.2
+ * 添加腾讯文档
+ *
  * 2024-01-11
  * 1.1
  * 添加 CSDN
@@ -37,6 +42,7 @@
      * 掘金 target
      * 天眼查 target
      * CSDN target
+     * 腾讯文档 url
      */
     const keys = ['target', 'url', 'link']
     // 获取到外部网站 url
