237bfb3ecd2d24604019570dfcd8cc92e778bbf7
yangxfan <18810287175@163.com>
2024-03-20 14:54:08 +0800

    update: 正在离开网站-添加OSCHINA

--- scripts/leaving-website-to-redirect.js
+++ scripts/leaving-website-to-redirect.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         正在离开网站，即将跳转到外部网站
 // @namespace    https://github.com/yangxfan/tampermonkey-extensions
-// @version      1.2
-// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN、腾讯文档
+// @version      1.3
+// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN、腾讯文档、OSCHINA
 // @author       yangxfan
 // @match        *://link.zhihu.com/*
 // @match        *://www.jianshu.com/go-wild*
@@ -12,12 +12,17 @@
 // @match        *://www.tianyancha.com/security/*
 // @match        *://link.csdn.net/?target=*
 // @match        *://docs.qq.com/scenario/link.html?url=*
+// @match        *://www.oschina.net/action/GoToLink?url=*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // @downloadURL  https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // ==/UserScript==
 
 /**
+ * 2024-03-20
+ * 1.3
+ * OSCHINA
+ *
  * 2024-03-07
  * 1.2
  * 添加腾讯文档
@@ -43,6 +48,7 @@
      * 天眼查 target
      * CSDN target
      * 腾讯文档 url
+     * OSCHINA url
      */
     const keys = ['target', 'url', 'link']
     // 获取到外部网站 url
