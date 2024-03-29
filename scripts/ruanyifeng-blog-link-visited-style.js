fae6e28a265de247c14e338ede674ab7ee01e2f9
yangxfan <18810287175@163.com>
2024-03-29 18:18:36 +0800

    update: 阮一峰博客文字颜色-优化匹配地址

--- scripts/ruanyifeng-blog-link-visited-style.js
+++ scripts/ruanyifeng-blog-link-visited-style.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         阮一峰博客里点击过的文章链接的文字颜色修改
 // @namespace    https://github.com/yangxfan/tampermonkey-extensions
-// @version      0.1
+// @version      0.2
 // @description  文章列表过多时，不好确认哪些文章点击访问过，该插件为了区分这种情况，突出已点击过的文章链接的文字颜色为红色
 // @author       yangxfan
-// @match        *://www.ruanyifeng.com/blog/*
+// @match        *://*.ruanyifeng.com/blog/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ruanyifeng.com
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/ruanyifeng-blog-link-visited-style.js
@@ -12,6 +12,10 @@
 // ==/UserScript==
 
 /**
+ * 2024-03-29
+ * 0.2
+ * 优化匹配地址
+ *
  * 2024-02-21
  * 0.1
  * 首次提交
