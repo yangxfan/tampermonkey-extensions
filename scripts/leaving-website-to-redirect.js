ac68d7269b7a87d73f4724761a1d84f5b3b48ee2
yangxfan <18810287175@163.com>
2024-03-29 13:54:10 +0800

    update: 正在离开网站-添加腾讯云开发者社区

--- scripts/leaving-website-to-redirect.js
+++ scripts/leaving-website-to-redirect.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         正在离开网站，即将跳转到外部网站
 // @namespace    https://github.com/yangxfan/tampermonkey-extensions
-// @version      1.3
-// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN、腾讯文档、OSCHINA
+// @version      1.4
+// @description  进入站点后，不需要手动点击“跳转”按钮，自动跳转到外部网站，支持知乎、简书、码云、少数派、掘金、天眼查、CSDN、腾讯文档、OSCHINA、腾讯云开发者社区
 // @author       yangxfan
 // @match        *://link.zhihu.com/*
 // @match        *://www.jianshu.com/go-wild*
@@ -13,12 +13,17 @@
 // @match        *://link.csdn.net/?target=*
 // @match        *://docs.qq.com/scenario/link.html?url=*
 // @match        *://www.oschina.net/action/GoToLink?url=*
+// @match        *://cloud.tencent.com/developer/tools/blog-entry?target=*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // @downloadURL  https://raw.githubusercontent.com/yangxfan/tampermonkey-extensions/main/scripts/leaving-website-to-redirect.js
 // ==/UserScript==
 
 /**
+ * 2024-03-29
+ * 1.4
+ * 添加腾讯云开发者社区
+ *
  * 2024-03-20
  * 1.3
  * OSCHINA
@@ -49,6 +54,7 @@
      * CSDN target
      * 腾讯文档 url
      * OSCHINA url
+     * 腾讯云开发者社区 target
      */
     const keys = ['target', 'url', 'link']
     // 获取到外部网站 url
